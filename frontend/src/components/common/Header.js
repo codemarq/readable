7a1de08002097c3052d003b22d44d252f10f97ba
John-Mike <jmmarquardt@gmail.com>
2018-02-26 07:39:18 -0500

    CHORE: restored header, bootstrap, jquery after computer died and had to re-image

--- frontend/src/components/common/Header.js
+++ frontend/src/components/common/Header.js
@@ -4,13 +4,28 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <nav>
-      <Link to="/" activeClassName="active">Home</Link>
-      {" | "}
-      <Link to="about" activeClassName="active">About</Link>
-      {" | "}
-      <Link to="posts" activeClassName="active">Posts</Link>
-    </nav>
+    <header>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <Link className="navbar-brand" to="/"><h1>Readable</h1></Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <Link className="nav-link active" to="/">Home <span className="sr-only">(current)</span></Link>
+            </li>
+            <li className="nav-item active">
+              <Link to="about" className="nav-link active">About</Link>
+            </li>
+            <li className="nav-item active">
+              <Link to="posts" className="nav-link active">Posts</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    
+    </header>
   )
 }
 
