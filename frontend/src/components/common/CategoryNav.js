8489679f9ad607a2bd25e2ea8cdce628a747d212
John-Mike <jmmarquardt@gmail.com>
2018-04-22 13:40:29 -0400

    CHORE: trying to fix header layout

--- frontend/src/components/common/CategoryNav.js
+++ frontend/src/components/common/CategoryNav.js
@@ -5,7 +5,7 @@ import FaPlusCircle from 'react-icons/lib/fa/plus-circle'
 
 const CategoryDropDown = () => {
 	return (
-		<div className="collapse navbar-collapse col-md-6" id="navbarSupportedContent">
+		<div className="collapse navbar-collapse" id="navbarSupportedContent">
 			<ul className="navbar-nav mr-auto">
 				<li className="nav-item active">
 					<Link className="nav-link active" to="/">Home <span className="sr-only">(current)</span></Link>
@@ -15,9 +15,6 @@ const CategoryDropDown = () => {
 				</li>
 				<li className="nav-item active">
 					<Link to="/posts" className="nav-link active">Posts</Link>
-				</li>
-				<li className="nav-item active">
-					<Link to="/createPost" className="btn btn-success"><FaPlusCircle/> New</Link>
 				</li>
 			</ul>
 		</div>
--- frontend/src/components/common/Header.js
+++ frontend/src/components/common/Header.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-// import FaPlusCircle from 'react-icons/lib/fa/plus-circle'
-import CategoryDropDown from './CategoryDropDown'
+import FaPlusCircle from 'react-icons/lib/fa/plus-circle'
+import CategoryNav from './CategoryNav'
 
 const Header = () => {
   return (
     <header className="bg-dark">
       <nav className="navbar container navbar-expand-lg navbar-dark bg-dark">
-        <Link className="navbar-brand col-md-6" to="/"><h1 className="light">Readable</h1></Link>
+        <Link className="navbar-brand col-md-4" to="/"><h1 className="light">Readable</h1></Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <CategoryDropDown/>
+        <CategoryNav className="col-md-4"/>
+        <div className="col-md-4">
+          <Link to="/createPost" className="btn btn-success"><FaPlusCircle/> New</Link>
+        </div>
       </nav>
     </header>
   )
